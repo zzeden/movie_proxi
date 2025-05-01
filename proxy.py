c03c41269dac70ce9b81cc95179a51b0fc448694
zzeden <Eden.zabarsky@gmail.com>
2025-05-01 16:11:54 +0300

    all time working

--- proxy.py
+++ proxy.py
@@ -20,15 +20,15 @@ def picture_fix(data: str):
     return new_data
 
 def main():
-    sock_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock_server.connect((SERVER_ADDRESS, SERVER_PORT))
-
-    sock_client = socket.socket()
-    sock_client.bind(('', CLIENT_PORT))
-    sock_client.listen(1)
-    conn_client, addr_client = sock_client.accept()
-
     while True:
+        sock_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock_server.connect((SERVER_ADDRESS, SERVER_PORT))
+
+        sock_client = socket.socket()
+        sock_client.bind(('', CLIENT_PORT))
+        sock_client.listen(1)
+        conn_client, addr_client = sock_client.accept()
+
         data_from_client = conn_client.recv(1024)
         if not data_from_client:
             break
@@ -42,8 +42,9 @@ def main():
         response_from_server = response_from_server.encode()
         conn_client.send(response_from_server)
 
-    conn_client.close()
-    sock_server.close()
+        sock_client.close()
+        conn_client.close()
+        sock_server.close()
 
 if __name__ == '__main__':
     main()
